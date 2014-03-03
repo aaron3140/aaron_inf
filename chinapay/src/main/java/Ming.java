b562facd0c3aae397833e7ff932f055504414967
aaromMing <shuangming.yang@gmail.com>
2014-03-04 00:39:33 +0800

    Ming test

---
+++ chinapay/src/main/java/Ming.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-3-4
+ * Time: 上午12:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ming {
+    public static void main(String[] args) {
+        System.out.println("xiaoming");
+    }
+
+    public void juntTest(){
+
+        System.out.println("is my junit test?");
+    }
+}
